925ed4cd80ce6fb444c763bfc086af873bb88a7e
Mursupaani <antti.pollanen89@gmail.com>
2025-07-09 21:56:30 +0300

    optimizing

--- srcs/client.c
+++ srcs/client.c
@@ -12,9 +12,10 @@
 
 #include "../incl/minitalk.h"
 
-static s_sigaction	initialize_sigaction(void);
-static void			send_message(const char *message, pid_t pid);
-static void			signal_handler(int signal, siginfo_t *info, void *context);
+s_sigaction	initialize_sigaction(void (*handler)(int, siginfo_t *, void *));
+void		send_message(const char *message, pid_t pid, s_sigaction sa);
+void		signal_handler(int signal, siginfo_t *info, void *context);
+void		signal_handler2(int signal, siginfo_t *info, void *context);
 
 static volatile sig_atomic_t g_signal_received;
 
@@ -26,30 +27,28 @@ int	main(int argc, char *argv[])
 	if (argc != 3)
 		return (EXIT_FAILURE);
 	server_pid = ft_atoi(argv[1]);
-	sa = initialize_sigaction();
-	send_message(argv[2], server_pid);
-	//FIXME:Handle this better
-	(void)sa;
+	sa = initialize_sigaction(&signal_handler);
+	send_message(argv[2], server_pid, sa);
 	return (0);
 }
 
 
-static s_sigaction	initialize_sigaction(void)
+s_sigaction	initialize_sigaction(void (*handler)(int, siginfo_t *, void *))
 {
 	s_sigaction	sa;
 
-	sigemptyset(&sa.sa_mask);
+	// sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
 	// sa.sa_flags = SA_RESTART;
 
-	sa.sa_sigaction = &signal_handler;
+	sa.sa_sigaction = handler;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	// sigaddset(&(sa->sa_mask), SIGINT);
 	return (sa);
 }
 
-static void	send_message(const char *message, pid_t pid)
+void	send_message(const char *message, pid_t pid, s_sigaction sa)
 {
 	int		strlen;
 	char	*strlen_str;
@@ -66,9 +65,11 @@ static void	send_message(const char *message, pid_t pid)
 		send_char_as_bits(strlen_str[i++], pid);
 	send_char_as_bits(strlen_str[i], pid);
 	free(strlen_str);
-	while (*message)
+	sa = initialize_sigaction(&signal_handler2);
+		while (*message)
 		send_char_as_bits(*message++, pid);
 	send_char_as_bits(*message, pid);
+	(void)sa;
 }
 
 void	signal_handler(int signal, siginfo_t *info, void *context)
@@ -78,13 +79,30 @@ void	signal_handler(int signal, siginfo_t *info, void *context)
 	if (signal == SIGUSR1)
 		g_signal_received = 1;
 	if (signal == SIGUSR2)
+	{
+		ft_printf("Server is busy. Please try again in a moment.\n");
 		exit(EXIT_FAILURE);
+	}
+}
+
+void	signal_handler2(int signal, siginfo_t *info, void *context)
+{
+	(void)context;
+	(void)info;
+	if (signal == SIGUSR1)
+		g_signal_received = 1;
+	if (signal == SIGUSR2)
+	{
+		ft_printf("Message sent.\n");
+		exit(EXIT_SUCCESS);
+	}
 }
 
 void	send_char_as_bits(unsigned char c, int pid)
 {
 	unsigned char	current_bit;
 	int				i;
+	int				current_signal;
 
 	current_bit = 0;
 	i = 8;
@@ -93,12 +111,12 @@ void	send_char_as_bits(unsigned char c, int pid)
 		g_signal_received = 0;
 		current_bit = c & 0b10000000;
 		if (current_bit)
-			kill(pid, SIGUSR1);
+			current_signal = SIGUSR1;
 		else
-			kill(pid, SIGUSR2);
+			current_signal = SIGUSR2;
+		kill(pid, current_signal);
 		while (!g_signal_received)
 			usleep(50);
-			// pause();
 		usleep(50);
 		c = c << 1;
 	}
